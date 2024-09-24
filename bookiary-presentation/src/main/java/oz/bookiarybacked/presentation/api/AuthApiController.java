358cc9d23c96dadbd669a0243ca6faf111ae83b2
uijin-j <uijin0531@gmail.com>
2024-09-24 21:50:06 +0900

    feat: 소셜로그인 API 엔드포인트 추가 (#4)

---
+++ bookiary-presentation/src/main/java/oz/bookiarybacked/presentation/api/AuthApiController.java
@@ -0,0 +1,59 @@
+package oz.bookiarybacked.presentation.api;
+
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import oz.bookiarybacked.application.auth.AuthService;
+import oz.bookiarybacked.application.auth.dto.response.LoginRes;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthApiController {
+
+	private final AuthService authService;
+
+	/**
+	 * 소셜 로그인 페이지로 리다이렉트하는 메서드 (ex. 카카오 로그인 페이지)
+	 * @param provider OAuth 제공자
+	 *  - 카카오: /api/oauth/kakao (🔗: https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api)
+	 */
+	@GetMapping("/oauth/{provider}")
+	public ResponseEntity<ApiResult<Void>> getLoginPage(
+		@PathVariable String provider
+	) {
+		URI redirectUri = authService.getLoginUrl(provider);
+		ApiResult<Void> result = ApiResult.of(HttpStatus.FOUND);
+
+		return ResponseEntity
+			.status(HttpStatus.FOUND)
+			.location(redirectUri)
+			.body(result);
+	}
+
+	/**
+	 * 로그인 요청을 처리하는 메서드
+	 * @param provider OAuth 제공자
+	 * @param code OAuth 인증 코드
+	 * @return 로그인 응답 (Access Token / Refresh Token)
+	 */
+	@PostMapping("/oauth/{provider}/login")
+	public ResponseEntity<ApiResult<LoginRes>> socialLogin(
+		@PathVariable String provider,
+		@RequestParam String code
+	) {
+		LoginRes response = authService.socialLogin(provider, code);
+		ApiResult<LoginRes> result = ApiResult.ok(response);
+
+		return ResponseEntity.ok(result);
+	}
+}
