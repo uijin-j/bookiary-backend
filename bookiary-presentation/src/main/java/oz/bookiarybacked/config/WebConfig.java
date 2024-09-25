229c682fb4503d5578d12e58f845972bf1a5829a
uijin-j <uijin0531@gmail.com>
2024-09-25 21:56:16 +0900

    chore: LoginArgumentResolver 추가 (#4)

---
+++ bookiary-presentation/src/main/java/oz/bookiarybacked/config/WebConfig.java
@@ -0,0 +1,19 @@
+package oz.bookiarybacked.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import oz.bookiarybacked.resolver.LoginArgumentResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new LoginArgumentResolver());
+	}
+
+}
