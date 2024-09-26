9b24d34f35856603479cef28d0fd1d40c27bca0b
uijin-j <uijin0531@gmail.com>
2024-09-26 16:29:54 +0900

    chore: 네이버 도서 검색 관련 설정 추가 (#6)

---
+++ bookiary-infrastructure/src/main/java/oz/bookiarybacked/config/properties/NaverSearchProperties.java
@@ -0,0 +1,11 @@
+package oz.bookiarybacked.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("book.naver")
+public record NaverSearchProperties(
+	String clientId,
+	String clientSecret,
+	String searchRequestUri
+) {
+}
