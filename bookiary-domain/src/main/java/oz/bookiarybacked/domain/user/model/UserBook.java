fa8972f690d6c71ad2284cc388186be3b7892792
uijin-j <uijin0531@gmail.com>
2024-09-27 18:17:46 +0900

    feat: Book, UserBook 엔티티 생성 (#8)

---
+++ bookiary-domain/src/main/java/oz/bookiarybacked/domain/user/model/UserBook.java
@@ -0,0 +1,44 @@
+package oz.bookiarybacked.domain.user.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import oz.bookiarybacked.common.BaseTimeEntity;
+
+@Entity
+@Table(name = "user_book")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserBook extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(name = "book_id")
+	private Long bookId;
+
+	public static UserBook of(User user, Long id) {
+		return UserBook.builder()
+			.user(user)
+			.bookId(id)
+			.build();
+	}
+}
