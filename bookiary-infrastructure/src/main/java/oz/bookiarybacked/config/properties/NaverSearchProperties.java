92e1c6d301b637eeda85a5bde3fc337cc1a5e2d3
uijin-j <uijin0531@gmail.com>
2024-09-27 18:20:54 +0900

    chore: search-detail-request-uri 설정 정보 추가 (#8)

--- bookiary-infrastructure/src/main/java/oz/bookiarybacked/config/properties/NaverSearchProperties.java
+++ bookiary-infrastructure/src/main/java/oz/bookiarybacked/config/properties/NaverSearchProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public record NaverSearchProperties(
 	String clientId,
 	String clientSecret,
-	String searchRequestUri
+	String searchRequestUri,
+	String searchDetailRequestUri
 ) {
 }
