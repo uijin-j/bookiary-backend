366f9fc9d49a4dff6445977953a7e4d717b2f754
uijin-j <uijin0531@gmail.com>
2024-09-30 01:37:58 +0900

    feat: 노트 생성 API 엔드포인트 추가

---
+++ bookiary-api/src/main/java/oz/bookiarybacked/domain/note/presentation/api/NoteApi.java
@@ -0,0 +1,35 @@
+package oz.bookiarybacked.domain.note.presentation.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import oz.bookiarybacked.common.presentation.annotation.Login;
+import oz.bookiarybacked.common.presentation.dto.ApiResult;
+import oz.bookiarybacked.domain.note.application.NoteService;
+import oz.bookiarybacked.domain.note.application.dto.request.NoteCreateRequest;
+
+@RestController
+@RequestMapping("/api/notes")
+@RequiredArgsConstructor
+public class NoteApi {
+	private final NoteService noteService;
+
+	@PostMapping
+	public ResponseEntity<ApiResult<Void>> createNote(
+		@Login Long loginId,
+		@Valid @RequestBody NoteCreateRequest request
+	) {
+		noteService.create(loginId, request);
+		ApiResult<Void> result = ApiResult.of(HttpStatus.CREATED);
+
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(result);
+	}
+}
