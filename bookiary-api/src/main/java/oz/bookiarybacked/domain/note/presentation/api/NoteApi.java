abcb5d0192f65418b126d6ac5029bde62960b486
uijin-j <uijin0531@gmail.com>
2024-09-30 02:44:35 +0900

    feat: 노트 수정 API 엔드포인트 추가 (#23)

--- bookiary-api/src/main/java/oz/bookiarybacked/domain/note/presentation/api/NoteApi.java
+++ bookiary-api/src/main/java/oz/bookiarybacked/domain/note/presentation/api/NoteApi.java
@@ -2,7 +2,9 @@ package oz.bookiarybacked.domain.note.presentation.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import oz.bookiarybacked.common.presentation.annotation.Login;
 import oz.bookiarybacked.common.presentation.dto.ApiResult;
 import oz.bookiarybacked.domain.note.application.NoteService;
 import oz.bookiarybacked.domain.note.application.dto.request.NoteCreateRequest;
+import oz.bookiarybacked.domain.note.application.dto.request.NoteUpdateRequest;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -30,6 +33,20 @@ public class NoteApi {
 
 		return ResponseEntity
 			.status(HttpStatus.CREATED)
+			.body(result);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ApiResult<Void>> update(
+		@Login Long loginId,
+		@PathVariable Long id,
+		@Valid @RequestBody NoteUpdateRequest request
+	) {
+		noteService.update(loginId, id, request);
+		ApiResult<Void> result = ApiResult.of(HttpStatus.OK);
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
 			.body(result);
 	}
 }
