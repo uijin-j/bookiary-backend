e886ec90f9a124f3129de50e3829903b051684d4
uijin-j <uijin0531@gmail.com>
2024-09-29 22:56:06 +0900

    feat: PermissionDeniedException 커스텀 예외 생성 (#12)

---
+++ bookiary-api/src/main/java/oz/bookiarybacked/common/exception/PermissionDeniedException.java
@@ -0,0 +1,8 @@
+package oz.bookiarybacked.common.exception;
+
+public class PermissionDeniedException extends RuntimeException {
+
+	public PermissionDeniedException(String message) {
+		super(message);
+	}
+}
